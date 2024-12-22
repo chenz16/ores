b38d4ba15e1a47bec3f8294d2a350a2e47d848bd
Chen Zhang <chen.zhang6@gmail.com>
2024-12-22 05:43:01 -0800

    make the imp match the theory

--- Reference_Implementation/Energy_Router/SMB/dq_control/c_imp_ref/lib_c/dq_controller_pid/dq_controller_pid.c
+++ Reference_Implementation/Energy_Router/SMB/dq_control/c_imp_ref/lib_c/dq_controller_pid/dq_controller_pid.c
@@ -39,7 +39,7 @@ void DQController_Update(DQController_State* state, DQController_Params* params)
     
     // Calculate feedback and feedforward components separately
     state->vd_fb = (params->kp_d * error_d + params->ki_d * state->integral_d) * CONTROLLER_SIGN;
-    state->vd_ff = (- params->R * state->id_meas - params->omega * params->L * state->iq_meas ) * CONTROLLER_SIGN;
+    state->vd_ff = (params->R * state->id_meas - params->omega * params->L * state->iq_meas ) * (CONTROLLER_SIGN);
     state->vd_ff += state->vd_grid;
     
     state->vd_out = state->vd_fb + state->vd_ff;
@@ -54,7 +54,7 @@ void DQController_Update(DQController_State* state, DQController_Params* params)
     
     // Calculate feedback and feedforward components separately
     state->vq_fb = (params->kp_q * error_q + params->ki_q * state->integral_q) * CONTROLLER_SIGN;
-    state->vq_ff = (- params->R * state->iq_meas + params->omega * params->L * state->id_meas ) * CONTROLLER_SIGN;
+    state->vq_ff = (params->R * state->iq_meas + params->omega * params->L * state->id_meas ) * (CONTROLLER_SIGN);
     state->vq_ff += state->vq_grid;
     state->vq_out = state->vq_fb + state->vq_ff;
 
